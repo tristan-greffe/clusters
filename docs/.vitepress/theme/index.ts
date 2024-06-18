392db33667a5647bc6d755e36f2c5cdfaeeafb16
tristan-greffe <tristan.greffe@hotmail.com>
2024-06-19 00:29:26 +0200

    docs: implementing an icon component for the kubernetes learning section

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
+import type { Theme } from 'vitepress'
+import KubernetesIcon from '../components/KubernetesIcon.vue'
 import './custom.css'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  enhanceApp({ app }) {
+    app.component('KubernetesIcon', KubernetesIcon)
+  }
+} satisfies Theme
