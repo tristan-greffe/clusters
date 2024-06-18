070c6dc2868ce5b105c7121372dc925e34b5a7f2
tristan-greffe <tristan.greffe@hotmail.com>
2024-06-18 19:37:10 +0200

    docs: implementing an icon component for the kubernetes learning section

---
+++ docs/.vitepress/components/KubernetesIcon.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="kubernetes-icon">
+    <img :src="icon" :alt="label" :width="width" :height="height" />
+    <span>{{ label }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Props
+defineProps({
+  icon: {
+    type: String,
+    required: true
+  },
+  label: {
+    type: String,
+    default: ''
+  },
+  width: {
+    type: Number,
+    default: 20
+  },
+  height: {
+    type: Number,
+    default: 20
+  }
+})
+</script>
+
+<style scoped>
+.kubernetes-icon {
+  display: flex;
+  align-items: center;
+}
+.kubernetes-icon img {
+  margin-right: 8px;
+}
+</style>
